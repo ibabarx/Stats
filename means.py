c7da1451663cd87e1d7770030c18cf042e88408f
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2023-02-04 12:45:58 +0500

    Update means.py

--- means.py
+++ means.py
@@ -43,4 +43,79 @@ def mean(dataframe,columns='all',decimal_points=2,plot=False,width = 0.8,bottom=
                 data_frame = pd.DataFrame(mean_dictionary.items())
             plotter()
             
+ 
+#################################################################################################################################################################
+#################################################################################################################################################################
+
+                                                               
+
+def trimmed_mean(dataframe,columns='all',trim = 0.05,decimal_points=2,plot=False,width = 0.8,bottom=None,align = 'center',
+         ticks_rotation=[90,0],color='green',edgecolor='green',linewidth=0):
+         
+    # An Inner function that is used to draw bar plots    
+    def plotter():
+        plt.figure()
+        plt.bar(x = data_frame[0],height=data_frame[1],width=width, bottom=bottom, align=align,color=color,edgecolor='green'
+                ,linewidth=linewidth)
+        plt.xticks(rotation=ticks_rotation[0])
+        plt.yticks(rotation=ticks_rotation[1])
         
+    # Empty Dictionary for use later, when converted to Pandas Dataframe to Display plots
+    mean_dictionary = {}
+        
+    # List of all features with int or float data type
+    list_of_int_columns = list(dataframe.select_dtypes(include=['int64','float64']).columns)
+    
+    # If the user wants to find the mean of all Features
+    if columns == 'all':
+        
+        # if barplot is required
+        if plot == False:
+            for column in list_of_int_columns:
+                series = dataframe[column]
+                no_to_trim = round(series.shape[0] * trim)
+                series = series.sort_values(ascending=True)
+                series = series[no_to_trim+1 : (series.shape[0]-no_to_trim)+1]
+                sol = str(sum(series) / (dataframe[column].shape[0]-(2*no_to_trim)))
+                print(column + " Mean = " + sol)
+                
+        # if barplot is not required 
+        else:
+            for column in list_of_int_columns:
+                series = dataframe[column]
+                no_to_trim = round(series.shape[0] * trim)
+                series = series.sort_values(ascending=True)
+                series = series[no_to_trim+1 : (series.shape[0]-no_to_trim)+1]
+                sol = str(sum(series) / (dataframe[column].shape[0]-(2*no_to_trim)))
+                print(column + " Mean = " + sol)
+                mean_dictionary[column] = sum(dataframe[column]) / dataframe[column].shape[0]
+                data_frame = pd.DataFrame(mean_dictionary.items())
+            plotter()
+                  
+    # If the user wants to find the mean of specific Features        
+    else:
+        # if barplot is required
+        if plot == False:
+            for column in columns:
+                series = dataframe[column]
+                no_to_trim = round(series.shape[0] * trim)
+                series = series.sort_values(ascending=True)
+                series = series[no_to_trim+1 : (series.shape[0]-no_to_trim)+1]
+                sol = str(sum(series) / (dataframe[column].shape[0]-(2*no_to_trim)))
+                print(column + " Mean = " + sol)
+        # if barplot is not required  
+        else:
+            for column in columns:
+                series = dataframe[column]
+                no_to_trim = round(series.shape[0] * trim)
+                series = series.sort_values(ascending=True)
+                series = series[no_to_trim+1 : (series.shape[0]-no_to_trim)+1]
+                sol = str(sum(series) / (dataframe[column].shape[0]-(2*no_to_trim)))
+                print(column + " Mean = " + sol)
+                mean_dictionary[column] = sum(dataframe[column]) / dataframe[column].shape[0]
+                data_frame = pd.DataFrame(mean_dictionary.items())
+            plotter()
+         
+         
+#################################################################################################################################################################
+#################################################################################################################################################################
