13191fdf03d346c9addfb40dea40543d0293e054
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2023-02-13 22:37:56 +0500

    Update iqr.py

--- iqr.py
+++ iqr.py
@@ -1,25 +1,27 @@
-def IQR(dataframe,columns='all'):
-    def quantile(dataframe,quantiles,columns='all'):
-        list_of_int_columns = list(dataframe.select_dtypes(include=['int64','float64']).columns)
+def iqr(dataframe,columns='all'):
+    list_of_int_columns = list(dataframe.select_dtypes(include=['int64','float64']).columns)
     
-        def sorting(column):
-            if type(column) == 'pandas.core.series.Series':
-                sorted_list = sorted(column.tolist())
-                return sorted_list
-            else:
-                sorted_list = sorted(column)
-                return sorted_list
-        
-        if columns == 'all':
-            for column in list_of_int_columns:
-                sorted_list = sorting(dataframe[column])
-                for quantile in quantiles:
-                    index = round(len(sorted_list)*(quantile))-1
-                    return sorted_list[index]
+    def sorting(column):
+        if type(column) == 'pandas.core.series.Series':
+            sorted_list = sorted(column.tolist())
+            return sorted_list
         else:
-            for column in columns:
-                sorted_list = sorting(dataframe[column])
-                for quantile in quantiles:
-                    index = round(len(sorted_list)*(quantile))-1
-                    return sorted_list[index]
-    print("IQR : " + str(quantile(data,[0.75],['avg_price_per_room']) - quantile(data,[0.25],['avg_price_per_room'])))
+            sorted_list = sorted(column)
+            return sorted_list
+        
+    if columns == 'all':
+        for column in list_of_int_columns:
+            sorted_list = sorting(dataframe[column])
+            q3_index = round(len(sorted_list)*(0.75))-1
+            q3 = sorted_list[q3_index]
+            q1_index = round(len(sorted_list)*(0.25))-1
+            q1 = sorted_list[q1_index]
+            print("IQR : " + str(q3-q1))
+    else:
+        for column in columns:
+            sorted_list = sorting(dataframe[column])
+            q3_index = round(len(sorted_list)*(0.75))-1
+            q3 = sorted_list[q3_index]
+            q1_index = round(len(sorted_list)*(0.25))-1
+            q1 = sorted_list[q1_index]
+            print("IQR : " + str(q3-q1))
