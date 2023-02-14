a6c821a8f13d7bf7444c97264de9ea3ec11a7d01
Abdullah Babar <104021733+ibabarx@users.noreply.github.com>
2023-02-14 10:42:18 +0500

    Update adjusted_r2.py

--- adjusted_r2.py
+++ adjusted_r2.py
@@ -1,8 +1,16 @@
 # FORMULA
 # 1 − (1 − R^2)( n − 1 / n − P − 1 )
 
-def ajdusted_rSquared(observed,predicted,p=5):
-    def r_squared(observed,predicted):
+def ajdusted_rSquared(observed,predicted,p=1):
+    
+    '''
+    observed = pass the column that contains the observed output values y. Format : Dataframe[y]
+    predicted = pass the column that contains the predicted output values Y. Format : Dataframe[Y]
+    p = number of parameters that were used to train the model and predict the values.
+        default : 1
+    '''   
+    
+    def r_squared(observed,predicted):          # An inner function that computes and returns R-square score of the model and the number of observations.
         observed_list = observed.tolist()
         predicted_list = predicted.tolist()
         n = len(observed_list)
@@ -10,8 +18,10 @@ def ajdusted_rSquared(observed,predicted,p=5):
         obs_pred_diff = [(observed_list[index] - predicted_list[index])**2 for index in range(n)]
         obs_mean_diff = [(observed_list[index] - mean)**2 for index in range(n)]
         r_squared = 1-(sum(obs_pred_diff)/sum(obs_mean_diff))
-        return n,r_squared
+        return n,r_squared          # Returns n(number of observations) & r_squared(R-square score)
+    
     n,r_squared = r_squared(observed,predicted)
-    adjusted_rSquare = 1-(1-r_squared)*((n-1)/(n-p-1))
-    print("R2 Score : " + str(r_squared))
-    print("Adjusted R2 Score : " + str(adjusted_rSquare))
+    adjusted_rSquare = 1-(1-r_squared)*((n-1)/(n-p-1))          # Calculating adjusted R-square score
+    
+    print("R2 Score : " + str(r_squared))           # print R_square score
+    print("Adjusted R2 Score : " + str(adjusted_rSquare))           # print adjusted R_square score
